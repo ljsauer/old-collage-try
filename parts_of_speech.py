391f932ee600482c310013830186d80965ca6594
Lillie Sauer <lilliejsauer@gmail.com>
2020-12-25 14:31:09 -0600

    Create PartsOfSpeech class and break down class methods

--- parts_of_speech.py
+++ parts_of_speech.py
@@ -4,20 +4,40 @@ import nltk
 from nltk.corpus import gutenberg
 
 
-def gutenberg_list_of_adjectives(text: str) -> List[str]:
-    """
-    :param text: The name of a text from Project Gutenberg
+class PartsOfSpeech:
 
-    Usage:
-    leaves_of_grass_adjectives = gutenberg_list_of_adjectives('whitman-leaves.txt')
-    """
-    adjective_tags = ['JJ', 'JJR', 'JJS']
-    text_body = gutenberg.raw(text)
-    parts_of_speech = nltk.pos_tag(
-        nltk.tokenize.word_tokenize(str(text_body).strip(r'\').split(\'\n'))
-    )
-    adjectives = [a[0] for a in parts_of_speech if a[1] in adjective_tags]
-    return [adj.strip(r'\\') for adj in adjectives]
+    def __init__(self, text: str):
+        """
+        :param text: The name of a .txt file from Project Gutenberg
+        """
+        self.text = text
+
+    def _process_parts_of_speech(self) -> List[tuple]:
+        raw_text = gutenberg.raw(self.text)
+        parts_of_speech = nltk.pos_tag(
+            nltk.tokenize.word_tokenize(str(raw_text).strip(r'\').split(\'\n'))
+        )
+        return parts_of_speech
+
+    def gutenberg_list_of_adjectives(self) -> List[str]:
+        """
+        Usage:
+        leaves_of_grass_adjectives = gutenberg_list_of_adjectives('whitman-leaves.txt')
+        """
+        adjective_tags = ['JJ', 'JJR', 'JJS']
+        pos_tags = self._process_parts_of_speech()
+        adjectives = [w[0] for w in pos_tags if w[1] in adjective_tags]
+        return [adj.strip(r'\\') for adj in adjectives]
+
+    def gutenberg_list_of_verbs(self) -> List[str]:
+        """
+        Usage:
+        leaves_of_grass_verbs = gutenberg_list_of_verbs('whitman-leaves.txt')
+        """
+        verb_tags = ['VN', 'VBN', 'VBD', 'VBG', 'VBP', 'VBZ']
+        pos_tags = self._process_parts_of_speech()
+        verbs = [w[0] for w in pos_tags if w[1] in verb_tags]
+        return [v.strip(r'\\') for v in verbs]
 
 
-print(gutenberg_list_of_adjectives('shakespeare-hamlet.txt')[:50])
+# TODO: Create Gutenberg class to super init from PartsOfSpeech
