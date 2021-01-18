9016159b51066829f3abe7efb9228e658f7740cb
ljsauer <lilliejsauer@gmail.com>
2021-01-18 15:10:42 -0600

    Fix web scraper tests

--- tests/test_web_scraper/test_image_search.py
+++ tests/test_web_scraper/test_image_search.py
@@ -8,9 +8,10 @@ class TestImageSearch(unittest.TestCase):
     def setUp(self) -> None:
         self.keyword = "coffee+mug"
         self.max_images = 4
+        download_path = os.path.join(os.path.abspath(''), 'app/downloads')
         self.img_search = ImageSearch(
             self.keyword,
-            '/Users/sauerl/source/wwt/capstone/app/downloads',
+            download_path,
             max_images=self.max_images
         )
 
@@ -18,7 +19,7 @@ class TestImageSearch(unittest.TestCase):
         self.img_search.download_google_images()
         self.assertTrue(len(
             [x for x in os.listdir(self.img_search.download_path)]
-        ) == self.max_images)
+            ) == self.max_images)
         self.assertNotIn('.gif', [image for image in os.listdir(self.img_search.download_path)])
 
     def tearDown(self) -> None:
