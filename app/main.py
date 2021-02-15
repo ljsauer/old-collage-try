2bf7557f7d3b0fa4bcfa65c5c219cb48cb2bcc79
Lillie Sauer <lilliejsauer@gmail.com>
2021-02-14 23:23:25 -0600

    Adjust error handling

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import cv2
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, Response
 from werkzeug.utils import redirect
 
 from app.computer_vision.image_collage import CollageGenerator
@@ -12,8 +12,8 @@ class UploadError(object):
     pass
 
 
-@app.route('/index')
-def start():
+@app.route('/main')
+def index():
     return render_template('index.html')
 
 
@@ -27,13 +27,16 @@ def uploader():
     if request.method == 'POST':
         f = request.files['file']
         text = str(f.read())
-        return collage_generator(text)
-    raise UploadError
+        return generate_collage(text)
+    return Response("Please try again with a new text file", status=406)
 
 
 @app.route('/generate')
-def collage_generator(text):
-    collage = CollageGenerator(text, num_words=10).create_collage()
+def generate_collage(text, num_words=15):
+    collage = CollageGenerator(text,
+                               num_words=num_words,
+                               img_per_word=int(75/num_words)
+                               ).create_collage()
     cv2.imwrite("static/image_collage.jpg", collage)
     render_template("image.html", image_collage='static/image_collage.jpg')
     return show_image()
@@ -41,4 +44,3 @@ def collage_generator(text):
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
-    start()
